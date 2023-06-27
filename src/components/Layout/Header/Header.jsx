b67e914379c440046c2ffc9f172c1d2b14cd18ef
Temur_Zhakibaev <temurzhakibaev@gmail.com>
2023-06-27 14:10:15 -0700

    added header

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import "./Header.scss";
 
 const Header = () => {
-  return (
-    <div>
-      x
-    </div>
-  )
-}
+	return (
+		<header className="header">
+			<button className="header__left">add post</button>
+			<div className="header__right">
+				<input placeholder="search by name or id" type="search" />
+				<select name="" id="" className={"header__right__select"}>
+					<option value="">almaz</option>
+					<option value="">almaz</option>
+					<option value="">almaz</option>
+					<option value="">almaz</option>
+				</select>
+			</div>
+		</header>
+	);
+};
 
-export default Header
+export default Header;
